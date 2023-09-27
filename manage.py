32bc09833c1612e16af8f75553a0a5f17a678f63
Ranu Patidar <ranupatidar4589@gmail.com>
2023-09-27 12:41:45 +0530

    c3

--- manage.py
+++ manage.py
@@ -19,7 +19,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
 
-        """ hello """
+        """ hello 1 """
     execute_from_command_line(sys.argv)
 
 
